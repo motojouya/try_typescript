7945425c809816eaf3b96b80181397ffee439704
Takuya Sugiyama @motojouya <motojouya@gmail.com>
2024-11-18 01:53:22 +0000

    err fix

--- neverthrow.ts
+++ neverthrow.ts
@@ -27,18 +27,34 @@ function callerFunc(val: number): Result<number, RangeError> {
     .andThen(pow(0.5));
 }
 
-function bind<O extends object, E2, A, K extends string>(key: K, func: (data: O) => Result<A, E2>) {
-  return function(beforeData: O): Result<O & { K: A }, E2> {
+function bind<N extends string, O extends object, E2, A>(key: Exclude<N, keyof O>, func: (data: O) => Result<A, E2>) {
+  return function(beforeData: O): Result<{ [K in N | keyof O]: K extends keyof O ? O[K] : A }, E2> {
     return func(beforeData).map(a => ({
       ...beforeData,
       [key]: a,
-    } as O & { K: A })); // 筆者ではうまいやり方がわからずasを利用
+    } as { [K in N | keyof O]: K extends keyof O ? O[K] : A })); // 筆者ではうまいやり方がわからずasを利用
   }
 }
 
 function callerFunc2(val: number): Result<number, RangeError> {
   return ok({ val: val + 10 })
-    .andThen(bind<{ val : number }, RangeError, number, 'divided'>('divided', ({ val }) => divide(2)(val)))
-    .andThen(bind<{ val : number, divided: number }, RangeError, number, 'powed'>('powed', ({ val }) => pow(0.5)(val)))
+    .andThen(bind('divided', ({ val }) => divide(2)(val)))
+    .andThen(bind('powed', ({ val }) => pow(0.5)(val)))
     .map(({ divided, powed }) => (divided * powed));
 }
+
+// function bind<O extends object, E2, A, K extends string>(key: K, func: (data: O) => Result<A, E2>) {
+//   return function(beforeData: O): Result<O & { K: A }, E2> {
+//     return func(beforeData).map(a => ({
+//       ...beforeData,
+//       [key]: a,
+//     } as O & { K: A })); // 筆者ではうまいやり方がわからずasを利用
+//   }
+// }
+// 
+// function callerFunc2(val: number): Result<number, RangeError> {
+//   return ok({ val: val + 10 })
+//     .andThen(bind<{ val : number }, RangeError, number, 'divided'>('divided', ({ val }) => divide(2)(val)))
+//     .andThen(bind<{ val : number, divided: number }, RangeError, number, 'powed'>('powed', ({ val }) => pow(0.5)(val)))
+//     .map(({ divided, powed }) => (divided * powed));
+// }
